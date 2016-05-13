42d00c00a82f37b7964b899d86fb4d10a32b27af
Andrej Beliakov <andbel@gmail.com>
2016-05-13 15:07:02 +0200

    refactor to sqlalchemy

--- crawl/spiders/fire.py
+++ crawl/spiders/fire.py
@@ -12,11 +12,17 @@ import logging
 # import urlparse
 import time
 from datetime import datetime
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.testing.schema import Column
+from sqlalchemy import Column, Integer, String, Boolean, create_engine
+from sqlalchemy import func
 import sys
 
 import sqlite3
 from natsort import natsort_keygen, ns
+
+Base = declarative_base()
 
 
 class FireSpider(scrapy.Spider):
@@ -28,21 +34,11 @@ class FireSpider(scrapy.Spider):
     conn = None
 
     def __init__(self):
-        self.connect_to_sqlite()
 
-    def connect_to_sqlite(self):
-        dbfile = u'sqlite3.db'
-        d = os.path.dirname(os.path.abspath(dbfile))
-        if not os.path.exists(d):
-            os.makedirs(d)
-        if os.path.isfile(dbfile):
-            self.conn = sqlite3.connect(dbfile)
-        else:
-            self.conn = sqlite3.connect(dbfile)
-            c = self.conn.cursor()
-            c.execute(
-                '''CREATE TABLE reports (id INTEGER PRIMARY KEY, address TEXT, content TEXT, district TEXT, url TEXT, time TEXT, title TEXT)''')
-            self.conn.commit()
+        engine = create_engine('sqlite:///sqlite.db', echo=True)
+        self.Session = sessionmaker(bind=engine)
+        Base.metadata.create_all(engine)
+        #self.connect_to_sqlite()
 
     def natural_sort(self, l):
         convert = lambda text: int(text) if text.isdigit() else text
@@ -56,16 +52,14 @@ class FireSpider(scrapy.Spider):
         return response.xpath('//div[@class="news-list-browse"]/ul/li/a/@href')
 
     def parse(self, response):
+        session = self.Session()
         reports = self.reports(response)
         # contentdict['id'] = re.search(r"([0-9]+)\/$", response.url).group(1)
 
         for report in reports:
-            report_id = (re.search(r"([0-9]+)\/$", report.extract()).group(1),)
-            check_id = \
-                list(
-                    self.conn.cursor().execute('''SELECT count(id) FROM reports WHERE id = ?''', report_id).fetchone())[
-                    0]
-            if check_id == 0:
+            report_id = re.search(r"([0-9]+)\/$", report.extract()).group(1)
+
+            if session.query(func.count(Report.id)).filter_by(id=int(report_id)).first()[0] == 0:
                 url = response.urljoin(report.extract())
                 logging.debug("Extracted report URL: {}".format(url))
                 yield scrapy.Request(url, self.parse_report_data)
@@ -95,6 +89,7 @@ class FireSpider(scrapy.Spider):
             #    yield scrapy.Request(url, self.parse)
 
     def parse_report_data(self, response):
+        session = self.Session()
         contentdict = {}
         article = response.xpath('//div[@class="news-single-item"]')
         article_content = article.xpath('.//p/text()')
@@ -105,21 +100,21 @@ class FireSpider(scrapy.Spider):
         article_start = 2
         if article_content[1].extract() == u' ': article_start = 3
         content_text = u''
-        contentdict['id'] = re.search(r"([0-9]+)\/$", response.url).group(1)
-        if contentdict['id'] == '3002': return
+        contentdict['id'] = int(re.search(r"([0-9]+)\/$", response.url).group(1))
+        if contentdict['id'] == 3002: return
         contentdict['url'] = response.url
         contentdict['title'] = title.extract()[0]
         contentdict['time'] = str(article_time)
-        contentdict['address'] = "".join(
+        contentdict['street'] = "".join(
             article_content[0].extract().replace(u'\xa0', u' ').strip())
-        contentdict['address'] = re.sub(r'^: ', '', contentdict['address'])
+        contentdict['street'] = re.sub(r'^: ', '', contentdict['street'])
         contentdict['district'] = "".join(
             article_content[article_start - 1].extract().replace(u'\xa0', u' ').strip())
         contentdict['district'] = re.sub(r'^: ', '', contentdict['district'])
-        # move address and/or district inside content if a certain length is exceeded
-        if len(contentdict['address']) > 40:
-            #content_text += ''.join(contentdict['address'] + '\r')
-            contentdict['address'] = ''
+        # move street and/or district inside content if a certain length is exceeded
+        if len(contentdict['street']) > 40:
+            #content_text += ''.join(contentdict['street'] + '\r')
+            contentdict['street'] = ''
             article_start -= 1
         if len(contentdict['district']) > 25:
             #content_text += ''.join(contentdict['district'] + '\r')
@@ -131,14 +126,11 @@ class FireSpider(scrapy.Spider):
         # replace CR by LF
         content_text = content_text.replace(u'\r', u'\x0a')
         contentdict['content'] = content_text
-        c = self.conn.cursor()
-        # c.execute('''CREATE TABLE reports (id integer primary key, address text, content text, district text, url text, time text, title text)''')
+        session.add(Report(id = contentdict['id'], street = contentdict['street'],
+                           content = contentdict['content'], district = contentdict['district'], url = contentdict['url'], time = contentdict['time'],
+                           title = contentdict['title']))
+        session.commit()
 
-        c.execute('INSERT INTO reports VALUES (?, ?, ?, ?, ?, ?, ?)', (
-            contentdict['id'], contentdict['address'], contentdict['content'], contentdict['district'],
-            contentdict['url'],
-            contentdict['time'], contentdict['title']))
-        self.conn.commit()
         reload(sys)
         sys.setdefaultencoding("unicode-escape")
         logging.debug(u''.join(json.dumps(contentdict, indent=True, ensure_ascii=False).replace(u'\\n', u'\n')))
@@ -159,3 +151,16 @@ class FireSpider(scrapy.Spider):
         for href in self.pages(response):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_report_pages)
+
+class Report(Base):
+    #                '''CREATE TABLE reports (id INTEGER PRIMARY KEY, street TEXT, content TEXT, district TEXT, url TEXT, time TEXT, title TEXT, kind TEXT)''')
+
+    __tablename__ = 'reports'
+    id = Column(Integer, primary_key=True)
+    street = Column(String)
+    content = Column(String)
+    district = Column(String)
+    url = Column(String)
+    time = Column(String)
+    title = Column(String)
+    kind = Column(String, default='fire')
