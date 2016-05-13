c21d7c119095e3cd4255e2ddce84217c8525424c
Andrej Beliakov <andbel@gmail.com>
2016-05-13 15:15:05 +0200

    improve json exporter

--- db_to_json.py
+++ db_to_json.py
@@ -11,6 +11,7 @@ conn = sqlite3.connect('sqlite3.db')
 cur = conn.cursor()
 cur.execute('select * from reports')
 with open('reports.json', 'w') as file:
-    r = [dict((cur.description[i][0], str(value).encode('utf-8')) \
+
+    r = [dict((cur.description[i][0], (lambda x: str(x).encode('utf-8') if cur.description[i][0] != "id" else x)(value)) \
               for i, value in enumerate(row)) for row in cur.fetchall()]
     json.dump(r, file, indent=True, ensure_ascii=False, sort_keys=True)
